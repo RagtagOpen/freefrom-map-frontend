f06c7a6d9e9c2c21a89114b92c63cb7280e8254e
Grant Stromgren <grant@grantstromgren.com>
2021-01-13 22:05:02 -0600

    Progress

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import SharedLayout from 'components/SharedLayout'
 import UsMap from 'components/UsMap'
+import Modal from "components/modal/Modal";
 
 export default function Home() {
     return (
         <SharedLayout>
             <UsMap />
+            <Modal target="state-updates" text="Sign up for state updates" title="Sign up for state updates">
+                <h1>Hello</h1>
+            </Modal>
         </SharedLayout>
     )
 }
