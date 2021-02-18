738a6ecb1dac95d3658547a372d1d97ec4d0394e
Danny (he/him) <DanielKim1@users.noreply.github.com>
2021-02-17 21:18:15 -0500

    split monorepo (#78)

--- admin/src/helpers/utils.js
+++
@@ -1,8 +0,0 @@
-export function findWithAttr(array, attr, value) {
-    for(var i = 0; i < array.length; i += 1) {
-        if(array[i][attr] === value) {
-            return i;
-        }
-    }
-    return -1;
-}
