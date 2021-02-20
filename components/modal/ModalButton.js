785b5a2ae69590e320d77f986db28c9da3afb618
Landon Reed <landontreed@gmail.com>
2021-02-19 22:25:01 -0500

    add states list (#55)

--- components/modal/ModalButton.js
+++ components/modal/ModalButton.js
@@ -5,7 +5,16 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Modal.module.css';
 
-export default function ModalButton({ target, text }) {
+// FIXME: Should this be renamed, since it defines a common button element that
+//  can be used for both modal buttons and link/href buttons for navigation?
+export default function ModalButton({ href, target, text }) {
+    if (href) {
+        return (
+            <a type="button" className={`btn btn-primary ${styles.button}`} href={href}>
+                <FontAwesomeIcon icon={ faArrowRight } className="mr-1" /> { text }
+            </a>
+        )
+    }
     return (
         <button type="button" className={`btn btn-primary ${styles.button}`} data-toggle="modal" data-target={ `#${target}` }>
             <FontAwesomeIcon icon={ faArrowRight } className="mr-1" /> { text }
@@ -14,6 +23,7 @@ export default function ModalButton({ target, text }) {
 }
 
 ModalButton.propTypes = {
+    href: PropTypes.string,
     target: PropTypes.string,
     text: PropTypes.string
 }
--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,49 @@ import React from 'react'
 import ReportMissingInfo from 'components/common/ReportMissingInfo'
 import ShareButtons from 'components/common/ShareButtons'
 import StateUpdates from 'components/common/StateUpdates'
+import PlaceholderText from "components/mock/Placeholder";
 import SharedLayout from 'components/SharedLayout'
+import StatesList from 'components/StatesList'
 import UsMap from 'components/UsMap'
 
 export default function Home() {
     return (
         <SharedLayout>
-            <UsMap />
-            <div className="d-flex">
-                <StateUpdates />
-                <ReportMissingInfo />
-                <ShareButtons className="ml-3" />
+            <div className="row">
+                <div className="col-12">
+                    <h1 className='mt-3'>
+                        The National Survivor Wealth Policy Map and Scorecard
+                    </h1>
+                    <p className='mb-5'>
+                        How well does your state support survivors&apos; financial security?
+                    </p>
+                </div>
+                <div className="col-12 col-md-3">
+                    <StatesList />
+                </div>
+                <div className="col-12 col-md-9">
+                    <UsMap />
+                </div>
+                <div className='col-12 mb-4'>
+                    <div className='float-right'>
+                        <div><small>Survivor wealth friendliness</small></div>
+                        <div id='legend' style={{height: '40px', width: '200px', backgroundColor: 'blue'}}/>
+                    </div>
+                </div>
+                <div className='col-12 mb-4'>
+                    <div className="ml-5 d-flex float-right">
+                        {/* FIXME: span is a hack for spacing */}
+                        <span className="mr-5">
+                            <StateUpdates />
+                        </span>
+                        <ReportMissingInfo />
+                        <ShareButtons className="ml-5" />
+                    </div>
+                </div>
+                <div className="col-12">
+                    <h1>About this tool</h1>
+                    <PlaceholderText />
+                </div>
             </div>
         </SharedLayout>
     )
