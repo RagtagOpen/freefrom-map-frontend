397ee1d0b17ccf3024cacc885adff9de04fd4a72
Landon Reed <landontreed@gmail.com>
2021-02-21 13:04:36 -0500

    move scorecard to component

--- pages/[state].js
+++ pages/[state].js
@@ -1,11 +1,8 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 import { useRouter } from 'next/router'
 import { regenCycle } from 'constants'
 
-import Modal from "components/modal/Modal";
 import SharedLayout from "components/SharedLayout";
 import Breadcrumbs from "components/common/Breadcrumbs";
 import ReportMissingInfo from 'components/common/ReportMissingInfo'
@@ -15,12 +12,14 @@ import ShareButtons from 'components/common/ShareButtons'
 import StateUpdates from 'components/common/StateUpdates'
 import Glossary from 'components/common/Glossary';
 import ModalButton from "components/modal/ModalButton";
+import Scorecard from 'components/Scorecard';
 
 export default function State() {
     const router = useRouter()
     const { state } = router.query
     const stateCapitalized = state ? state[0].toUpperCase() + state.substr(1) : ''
     const imageUrl = "https://via.placeholder.com/600x370"
+    // TODO: Replace with data from back end.
     const categories = [
         {
             active: true,
@@ -51,18 +50,10 @@ export default function State() {
                 </div>
                 <div className="col-12 col-md-7">
                     <div className="float-right">
+                        {/* FIXME: add url to report */}
                         <ModalButton href="#" text="Download report" />
                     </div>
-                    <div className='mt-5 mb-3'>
-                        <span className='mr-2' style={{fontSize: '1.2em', fontWeight: 400, textTransform: 'uppercase'}}>Overall:</span>
-                        <ScoreLabel score={2}/>
-                    </div>
-                    <p><em>This state prioritizes...</em></p>
-                    <div className="accordion" id="accordionExample">
-                        {categories.map(category => (
-                            <CategoryCard category={category} key={category.id} />
-                        ))}
-                    </div>
+                    <Scorecard categories={categories} overallScore={2} />
                     <h2>Understanding this report</h2>
                     <ScoringGuide />
                     <Glossary />
@@ -70,29 +61,6 @@ export default function State() {
                 </div>
             </div>
         </SharedLayout>
-    )
-}
-
-const CategoryCard = ({ category }) => {
-    const collapseId = `collapse-${category.id}`
-    const headingId = `heading-${category.id}`
-    return (
-        <div style={{borderTop: '1px solid black'}}>
-            <div className="pt-3 pb-3" id={headingId} type="button" data-toggle="collapse" data-target={`#${collapseId}`} aria-expanded="false" aria-controls={collapseId}>
-                <h2 className="m-0" style={{textTransform: 'uppercase', fontSize: '0.75em'}}>
-                    {category.title}
-                    <div className="float-right">
-                        <span className='mr-3'><ScoreLabel score={2}/></span>
-                        <FontAwesomeIcon className='fa-2x' icon={ faCaretDown } />
-                    </div>
-                </h2>
-            </div>
-            <div id={collapseId} className="collapse pb-3" aria-labelledby={headingId} data-parent="#accordionExample">
-                <div className="p-0 card-body small font-italic">
-                    {category.help_text}
-                </div>
-            </div>
-        </div>
     )
 }
 
