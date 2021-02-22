99d60ef0e65d5dd9c8b51cbc9d7108f7afe3d6c9
Landon Reed <landontreed@gmail.com>
2021-02-21 19:23:00 -0500

    add take action to state page (#92)

--- pages/[state].js
+++ pages/[state].js
@@ -5,11 +5,12 @@ import { regenCycle } from 'constants'
 
 import SharedLayout from "components/SharedLayout";
 import Breadcrumbs from "components/common/Breadcrumbs";
+import styles from 'components/common/Common.module.css';
 import ReportMissingInfo from 'components/common/ReportMissingInfo'
-import ScoreLabel from 'components/common/ScoreLabel'
 import ScoringGuide from 'components/common/ScoringGuide'
 import ShareButtons from 'components/common/ShareButtons'
 import StateUpdates from 'components/common/StateUpdates'
+import TakeAction from 'components/common/TakeAction'
 import Glossary from 'components/common/Glossary';
 import ModalButton from "components/modal/ModalButton";
 import Scorecard from 'components/Scorecard';
@@ -38,26 +39,31 @@ export default function State() {
     ]
     return (
         <SharedLayout>
-            <Breadcrumbs currentPageTitle={ state } />
-            <h1>{ stateCapitalized } Survivor Wealth Policy Report</h1>
-            <p>How does { stateCapitalized } measure up to supporting survivor wealth?</p>
-            <div className="row">
-                <div className="col-12 col-md-4">
-                    <img className="img-fluid" src={imageUrl} />
-                    <StateUpdates />
-                    <ReportMissingInfo />
-                    <ShareButtons className="mb-5" />
-                </div>
-                <div className="col-12 col-md-7">
-                    <div className="float-right">
-                        {/* FIXME: add url to report */}
-                        <ModalButton href="#" text="Download report" />
+            <div className='state-page'>
+                <Breadcrumbs currentPageTitle={ state } />
+                <h1>{ stateCapitalized } Survivor Wealth Policy Report</h1>
+                <p>How does { stateCapitalized } measure up to supporting survivor wealth?</p>
+                <div className="row">
+                    <div className="col-12 col-md-4">
+                        <img className="img-fluid" src={imageUrl} />
+                        <StateUpdates />
+                        <ReportMissingInfo />
+                        <ShareButtons className="mb-5" />
                     </div>
-                    <Scorecard categories={categories} overallScore={2} />
-                    <h2>Understanding this report</h2>
-                    <ScoringGuide />
-                    <Glossary />
-                    <h2>Take action</h2>
+                    <div className="col-12 col-md-7">
+                        <div className="float-right">
+                            {/* FIXME: add url to report */}
+                            <ModalButton href="#" text="Download report" />
+                        </div>
+                        <Scorecard categories={categories} overallScore={2} />
+                        <div className="understanding-report">
+                            <h2 className="mb-0" >Understanding this report</h2>
+                            <ScoringGuide />
+                            <Glossary />
+                            <ModalButton href="/methodology" text="Full methodology" />
+                        </div>
+                        <TakeAction />
+                    </div>
                 </div>
             </div>
         </SharedLayout>
