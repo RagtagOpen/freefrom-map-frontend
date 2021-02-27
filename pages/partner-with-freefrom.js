e67b65569905c469b5627b52bc30dcba4a08dc80
Sarah Jaisser Green <sjaissergreen@gmail.com>
2021-02-26 22:31:14 -0800

    Update Partner with FreeFrom form
    Use underscores consistently for form fields/values.

--- pages/partner-with-freefrom.js
+++ pages/partner-with-freefrom.js
@@ -27,7 +27,7 @@ export default function PartnerWithFreefrom() {
                 <h1>Partner with FreeFrom</h1>
                 <p>Is your organization interested in partnering with FreeFrom? Please fill out the following information and we will get back to you as soon as possible.</p>
                 <Form
-                    path="partner_with_freefrom"
+                    path="partner-with-freefrom"
                     form={props => (
                         <form className="col-12 col-lg-8 mb-5">
                             <Input label="Your name" name="name" required={ true } />
@@ -38,20 +38,20 @@ export default function PartnerWithFreefrom() {
                             <Select label="Your state" name="state" required={ true } options={ states }/>
                             <div className="form-group">
                                 <FormLabel required={ true }>How would you like to partner with FreeFrom?</FormLabel>
-                                <Checkbox name="goals" value="policy-innovation-sprint">Request a policy innovation sprint</Checkbox>
-                                <Checkbox name="goals" value="help-drafting-legislation">Help with drafting legislation</Checkbox>
-                                <Checkbox name="goals" value="plan-event-or-webinar">Plan a policy related event or webinar</Checkbox>
-                                <Checkbox name="goals" value="pass-survivor-wealth-centered-legislation">Pass survivor wealth centered legislation in my state</Checkbox>
+                                <Checkbox name="goals" value="policy_innovation_sprint">Request a policy innovation sprint</Checkbox>
+                                <Checkbox name="goals" value="help_drafting_legislation">Help with drafting legislation</Checkbox>
+                                <Checkbox name="goals" value="plan_event_or_webinar">Plan a policy related event or webinar</Checkbox>
+                                <Checkbox name="goals" value="pass_survivor_wealth_centered_legislation">Pass survivor wealth centered legislation in my state</Checkbox>
                             </div>
-                            {props.values.goals && props.values.goals.indexOf("pass-survivor-wealth-centered-legislation") !== -1 &&
+                            {props.values.goals && props.values.goals.indexOf("pass_survivor_wealth_centered_legislation") !== -1 &&
                               <div className="form-group">
                                   <FormLabel>What phase of the process are you in?</FormLabel>
-                                  <Checkbox name="process_phase" value="need-guidance">I need guidance on centering my organization’s policy priorities to be more survivor-wealth informed</Checkbox>
-                                  <Checkbox name="process_phase" value="have-an-idea">I have a policy idea but need help with getting started</Checkbox>
-                                  <Checkbox name="process_phase" value="developed-a-coalition">I’ve developed a coalition or a network to support my policy idea</Checkbox>
-                                  <Checkbox name="process_phase" value="spoke-with-state">I’ve spoken with my state representative about my policy idea</Checkbox>
-                                  <Checkbox name="process_phase" value="ready-to-introduce">My state representative is ready to introduce my policy idea into a bill</Checkbox>
-                                  <Checkbox name="process_phase" value="finish-line">I need help getting a bill to the finish line</Checkbox>
+                                  <Checkbox name="process_phase" value="need_guidance">I need guidance on centering my organization’s policy priorities to be more survivor-wealth informed</Checkbox>
+                                  <Checkbox name="process_phase" value="have_an_idea">I have a policy idea but need help with getting started</Checkbox>
+                                  <Checkbox name="process_phase" value="developed_a_coalition">I’ve developed a coalition or a network to support my policy idea</Checkbox>
+                                  <Checkbox name="process_phase" value="spoke_with_state">I’ve spoken with my state representative about my policy idea</Checkbox>
+                                  <Checkbox name="process_phase" value="ready_to_introduce">My state representative is ready to introduce my policy idea into a bill</Checkbox>
+                                  <Checkbox name="process_phase" value="finish_line">I need help getting a bill to the finish line</Checkbox>
                               </div>
                             }
                             <Submit />
