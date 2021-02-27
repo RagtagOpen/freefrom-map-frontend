27543530f6df95fb2336858ff8064efc27a0c915
Sarah Jaisser Green <sjaissergreen@gmail.com>
2021-02-26 22:29:47 -0800

    Update Report Missing Info form
    Add email type to input. Use underscores consistently for form fields/values.

--- components/common/ReportMissingInfo.js
+++ components/common/ReportMissingInfo.js
@@ -12,12 +12,11 @@ const ReportMissingInfo = () => (
             With the help of some truly incredible pro bono partners, FreeFrom was able to complete research on policies impacting survivors’ financial security in every state and D.C. One of our priorities is to ensure that all of the information in our tool is correct and up-to-date. To that end, we welcome any correction on inaccurate or incomplete information. Thank you in advance for helping us make the Policy Map and Scorecard the best tool it can possibly be!
         </p>
         <Form
-            path="report_missing_info"
+            path="report-missing-info"
             form={() => (
                 <form>
-                    <Input name="what-is-outdated" required={ true } label="What information is missing or outdated?
-    (Please provide as much information as you can.)"/>
-                    <Input name="email" label="Your email (optional)" />
+                    <Input name="missing_or_outdated_info" label="What information is missing or outdated? (Please provide as much information as you can.)" required={ true } />
+                    <Input name="email" label="Your email (optional)" type="email" />
                     <Submit />
                 </form>
             )}
