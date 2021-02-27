dd3523c1653ac803341471a797651f715439230d
Sarah Jaisser Green <sjaissergreen@gmail.com>
2021-02-26 22:26:00 -0800

    Update Give Feedback form
    Use props to add "Please describe..." for "Other" option. Fix planned_use Checkbox name and values. Use underscores consistently for form fields/values.

--- pages/give-feedback.js
+++ pages/give-feedback.js
@@ -30,22 +30,25 @@ export default function GiveFeedback() {
                 <p>The Policy Map and Scorecard will only be effective if it is useful to our users. We want to hear from you how you used the tool and whether it was helpful. If you didn’t find the tool helpful, feel free to share how we can make the necessary improvements. Thanks so much for sharing your feedback and for helping us make the Policy Map and Scorecard the best tool it can possibly be!</p>
                 <Form
                     className="col-12 col-lg-8 mb-5"
-                    path="feedback"
-                    form={() => (
+                    path="give-feedback"
+                    form={props => (
                         <form>
-                            <Select label="Was this tool useful?" name="tool-useful" required={ true } options={ toolUseful }/>
-                            <Input label="Can you tell us more about how the tool was or was not useful for you?" required={ true } />
-                            <Select label="Did you learn anything about policies related to survivor wealth from the tool?" name="learn-from-tool" required={ true } options={ learnFromTool }/>
+                            <Select label="Was this tool useful?" name="tool_useful" required={ true } options={ toolUseful }/>
+                            <Input label="Can you tell us more about how the tool was or was not useful for you?" name="tool_useful_details" required={ true } />
+                            <Select label="Did you learn anything about policies related to survivor wealth from the tool?" name="learn_from_tool" required={ true } options={ learnFromTool }/>
                             <div className="form-group">
                                 <FormLabel required={ true }>How do you plan to use this tool?</FormLabel>
-                                <Checkbox name="inform-org-priorities">Inform my organization’s or state coalition’s policy priorities</Checkbox>
-                                <Checkbox name="inform-state-priorities">Inform my state’s policy priorities</Checkbox>
-                                <Checkbox name="inform-officials">Inform elected officials in my state</Checkbox>
-                                <Checkbox name="advocacy-tool">As an advocacy tool</Checkbox>
-                                <Checkbox name="self-educating">Self-educating</Checkbox>
-                                <Checkbox name="other">Other</Checkbox>
+                                <Checkbox name="planned_use" value="inform_org_priorities">Inform my organization’s or state coalition’s policy priorities</Checkbox>
+                                <Checkbox name="planned_use" value="inform_state_priorities">Inform my state’s policy priorities</Checkbox>
+                                <Checkbox name="planned_use" value="inform_officials">Inform elected officials in my state</Checkbox>
+                                <Checkbox name="planned_use" value="advocacy_tool">As an advocacy tool</Checkbox>
+                                <Checkbox name="planned_use" value="self_educating">Self-educating</Checkbox>
+                                <Checkbox name="planned_use" value="other">Other</Checkbox>
                             </div>
-                            <Input label="What can be improved or changed?" required={ true } />
+                            {props.values.planned_use && props.values.planned_use.indexOf("other") !== -1 &&
+                              <Input label="Please describe..." name="planned_use_other"/>
+                            }
+                            <Input label="What can be improved or changed?" name="improve_or_change" required={ true } />
                             <Submit />
                         </form>
                     )}
