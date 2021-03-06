af4b07250d3f04c7ff65b18380798b75952810c5
Landon Reed <landontreed@gmail.com>
2021-03-06 10:00:46 -0500

    fix bad merge

--- pages/states/[state].js
+++ pages/states/[state].js
@@ -1,11 +1,9 @@
 import React from "react";
 
 import { useRouter } from 'next/router'
-import { regenCycle } from 'constants'
 
 import SharedLayout from "components/SharedLayout";
 import Breadcrumbs from "components/common/Breadcrumbs";
-import styles from 'components/common/Common.module.css';
 import ReportMissingInfo from 'components/common/ReportMissingInfo'
 import ScoringGuide from 'components/common/ScoringGuide'
 import ShareButtons from 'components/common/ShareButtons'
@@ -22,26 +20,31 @@ function State({ categories, stateData }) {
     const imageUrl = "https://via.placeholder.com/600x370"
     return (
         <SharedLayout>
-            <Breadcrumbs currentPageTitle={ state } />
-            <h1>{ name } Survivor Wealth Policy Report</h1>
-            <p>How does { name } measure up to supporting survivor wealth?</p>
-            <div className="row">
-                <div className="col-12 col-md-4">
-                    <img className="img-fluid" src={imageUrl} />
-                    <StateUpdates />
-                    <ReportMissingInfo />
-                    <ShareButtons className="mb-5" />
-                </div>
-                <div className="col-12 col-md-7">
-                    <div className="float-right">
-                        {/* FIXME: add url to report */}
-                        <ModalButton href="#" text="Download report" />
+            <div className='state-page'>
+                <Breadcrumbs currentPageTitle={ state } />
+                <h1>{ name } Survivor Wealth Policy Report</h1>
+                <p>How does { name } measure up to supporting survivor wealth?</p>
+                <div className="row">
+                    <div className="col-12 col-md-4">
+                        <img className="img-fluid" src={imageUrl} />
+                        <StateUpdates />
+                        <ReportMissingInfo />
+                        <ShareButtons className="mb-5" />
                     </div>
-                    <Scorecard categories={categories} stateData={stateData} />
-                    <h2>Understanding this report</h2>
-                    <ScoringGuide />
-                    <Glossary />
-                    <h2>Take action</h2>
+                    <div className="col-12 col-md-7">
+                        <div className="float-right">
+                            {/* FIXME: add url to report */}
+                            <ModalButton href="#" text="Download report" />
+                        </div>
+                        <Scorecard categories={categories} stateData={stateData} />
+                        <div className="understanding-report">
+                            <h2 className="mb-0" >Understanding this report</h2>
+                            <ScoringGuide />
+                            <Glossary />
+                            <ModalButton href="/methodology" text="Full methodology" />
+                        </div>
+                        <TakeAction showPartnerLink />
+                    </div>
                 </div>
             </div>
         </SharedLayout>
