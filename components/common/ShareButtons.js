0ee7fe2347ba0de4092581dc4d05bc3f510aa489
Sarah Jaisser Green <sjaissergreen@gmail.com>
2021-03-08 11:05:10 -0800

    Add share URLs for Facebook and Twitter

--- components/common/ShareButtons.js
+++ components/common/ShareButtons.js
@@ -5,12 +5,16 @@ import { faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const ShareButtons = ({ className }) => (
     <div className={ className }>
-        <button type="button" className="btn btn-sm btn-primary mr-2">
-            <FontAwesomeIcon icon={ faFacebook } className="mr-1" /> Share
-        </button>
-        <button type="button" className="btn btn-sm btn-primary" style={{backgroundColor: '#1b95e0', borderColor: '#1b95e0'}}>
-            <FontAwesomeIcon icon={ faTwitter } className="mr-1" /> Tweet
-        </button>
+        <a href="https://www.facebook.com/sharer/sharer.php?u=https://freefrom-map-frontend.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+            <button type="button" className="btn btn-sm btn-primary mr-2">
+                <FontAwesomeIcon icon={ faFacebook } className="mr-1" /> Share
+            </button>
+        </a>
+        <a href="https://twitter.com/intent/tweet?text=Check%20out%20the%20new%20National%20Survivor%20Wealth%20Policy%20Map%20and%20Scorecard%20from%20@freefromorg!&url=https://freefrom-map-frontend.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+            <button type="button" className="btn btn-sm btn-primary" style={{backgroundColor: '#1b95e0', borderColor: '#1b95e0'}}>
+                <FontAwesomeIcon icon={ faTwitter } className="mr-1" /> Tweet
+            </button>
+        </a>
     </div>
 )
 
