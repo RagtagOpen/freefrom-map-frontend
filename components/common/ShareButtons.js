191085732bcd0e1e429160d6c1a1cd624d46d667
Sarah Jaisser Green <sjaissergreen@gmail.com>
2021-03-09 17:12:56 -0800

    Remove unnecessary button element for Twitter
    Co-authored-by: Danny (he/him) <DanielKim1@users.noreply.github.com>

--- components/common/ShareButtons.js
+++ components/common/ShareButtons.js
@@ -13,10 +13,13 @@ const ShareButtons = ({ className }) => (
         >
             <FontAwesomeIcon icon={ faFacebook } className="mr-1" /> Share
         </a>
-        <a href="https://twitter.com/intent/tweet?text=Check%20out%20the%20new%20National%20Survivor%20Wealth%20Policy%20Map%20and%20Scorecard%20from%20@freefromorg!&url=https://freefrom-map-frontend.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-            <button type="button" className="btn btn-sm btn-primary" style={{backgroundColor: '#1b95e0', borderColor: '#1b95e0'}}>
-                <FontAwesomeIcon icon={ faTwitter } className="mr-1" /> Tweet
-            </button>
+        <a
+            className="btn btn-sm btn-primary"
+            href="https://twitter.com/intent/tweet?text=Check%20out%20the%20new%20National%20Survivor%20Wealth%20Policy%20Map%20and%20Scorecard%20from%20@freefromorg!&url=https://freefrom-map-frontend.herokuapp.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <FontAwesomeIcon icon={ faTwitter } className="mr-1" /> Tweet
         </a>
     </div>
 )
