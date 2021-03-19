e6c2aa115a5a31540083734b42aaab7b8f172c8d
Sarah Jaisser Green <sjaissergreen@gmail.com>
2021-03-18 22:21:24 -0700

    Add snapshot and states to watch sections to homepage

--- pages/index.js
+++ pages/index.js
@@ -66,14 +66,43 @@ function Home({ states }) {
             </div>
 
             <>
-                
-                <h2>Important information</h2>
+
+                <h2 className='mt-5'>Snapshot of Survivor Financial Security Policies By State</h2>
+                <img className="img-fluid" src="/images/snapshot-by-state.png" />
+
+                <h2 className='mt-5'>Snapshot of Survivor Financial Security Policies By Category</h2>
+                <img className="img-fluid" src="/images/snapshot-by-category.png" />
+
+                <h2 className='mt-5'>States to Watch</h2>
+                <div className="d-flex justify-content-lg-around justify-content-center align-items-center flex-column flex-md-row p-2">
+                    <div className='d-flex flex-column flex-md-row justify-content-md-between' style={{minWidth: '60%'}}>
+                        <div className='pr-3'>
+                            <h3 className="mb-3">Maine</h3>
+                            <img className="img-fluid mb-3" src="/images/state-to-watch-1.png" />
+                            <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div className='pr-3'>
+                            <h3 className="mb-3">Washington</h3>
+                            <img className="img-fluid mb-3" src="/images/state-to-watch-2.png" />
+                            <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div className='pr-3'>
+                            <h3 className="mb-3">Colorado, Illinois, and Nevada</h3>
+                            <img className="img-fluid mb-3" src="/images/state-to-watch-3.png" />
+                            <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <h2 className='mt-5'>Important information</h2>
                 <p>The National Survivor Financial Security Policy Map and Scorecard evaluates legislation in each state that has either been passed or enacted. While we understand that there are an endless number of policies and issues that impact survivors’ ability to build wealth, we’ve selected policies that we have determined are most directly linked to a survivor’s ability to build and maintain financial security. This is a living tool, which means it will be regularly updated and expanded. Be sure to read the full methodology to learn more!</p>
-                <h2>Methodology</h2>
+
+                <h2 className='mt-5'>Methodology</h2>
                 <p>We evaluated each state based on whether and to what extent its policies aligned with and provided the same protections as our set of model policies, the characteristics of which are included in each state’s scorecard. The scorecard only includes state-level policies and does not capture local policies or those that can only be addressed on the federal level.</p>
                 <p>While survivors’ ability to build financial security is impacted by a wide range of policies, we chose the thirteen policy categories currently included within our scorecard after determining that they were most directly connected with survivors’ ability to protect their assets and build and save wealth.</p>
                 <p>While practice and policy may differ, the scorecard only measures policies as they are codified in state laws.</p>
                 <ModalButton text="Full Methodology" href="/methodology"/>
+
             </>
         </SharedLayout>
     );
