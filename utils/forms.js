a187c2cc88b5be894ae74677a9333fc71cca1729
Landon Reed <landontreed@gmail.com>
2021-03-25 17:47:00 -0400

    clean up form status component

--- utils/forms.js
+++ utils/forms.js
@@ -1,22 +1,27 @@
+import React from 'react'
+
 export const checkFormStatus = (props) => {
-    if (props.status) {
-        if (props.status.success) {
-            return <p>
-          Thanks for your response! Someone will be in touch with you soon.
-                <button onClick={props.resetForm}>
-            Reset
-                </button>
-            </p>
-        } else {
-            return <p>
-        Error submitting form! Please try again later.
-                <button onClick={props.resetForm}>
-          Reset
-                </button>
-            </p>
-        }
+    // If status has not been updated, form has not yet been submitted.
+    if (!props.status) return null
+    if (props.status.success) {
+        return (
+            <div className="col-12 col-lg-8 mb-5 mb-5 mt-5 alert alert-success" role="alert">
+                <strong>Thanks for your response!</strong> Someone will be in touch with you soon.
+                <a type="button" className="ml-1 alert-link" onClick={props.resetForm}>
+                Submit another response
+                </a>
+            </div>
+        )
+    } else {
+        return (
+            <div className="col-12 col-lg-8 mb-5 mb-5 mt-5 alert alert-danger" role="alert">
+                <strong>Error submitting form!</strong> Please try again later.{" "}
+                <a type="button" className="ml-1 alert-link" onClick={props.resetForm}>
+                  Try again
+                </a>
+            </div>
+        )
     }
-    return null
 }
 
 export const submitForm = path => {
