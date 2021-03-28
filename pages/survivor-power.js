f032550db202410a8b131d75a2499b6246f2f5ac
Landon Reed <landontreed@gmail.com>
2021-03-28 10:06:54 -0400

    Reset form on submission (#187)

--- pages/survivor-power.js
+++ pages/survivor-power.js
@@ -13,7 +13,7 @@ import Submit from "components/forms/Submit";
 
 import TakeAction from "components/common/TakeAction";
 
-import { submitForm } from 'utils'
+import { checkFormStatus, submitForm } from 'utils'
 
 export default function BuildCollectiveSurvivorPower() {
 
@@ -28,40 +28,44 @@ export default function BuildCollectiveSurvivorPower() {
                 <p>Survivors are often left out of the policy-making process, even when the policies being considered will directly impact our lives. At FreeFrom, we want to change that! We are working to create spaces and materials to support survivors who want to play an active role in creating and advocating for policies in their state that will improve their lives and the lives of their families.</p>
                 <p>Are you a survivor interested in being more involved in policy advocacy in your state? If so, please take a few moments to fill out this information and we will get back to you as soon as possible.</p>
                 <Formik initialValues={{}} onSubmit={submitForm("survivor-power")}>
-                    {props => (
-                        <Form className="col-12 col-lg-8 mb-5">
-                            <Input label="Your name" name="name" required={ true } />
-                            <Input label="Your pronouns" name="pronouns" required={ true } />
-                            <div className="form-group">
-                                <FormLabel required={ true }>Which of the following are you interested in? (Select as many as you'd like.)</FormLabel>
-                                <Checkbox name="interest_area" value="be_policy_advocate">Learning how to be a policy advocate in my state</Checkbox>
-                                <Checkbox name="interest_area" value="connect_with_survivors">Connecting with other survivors in my state</Checkbox>
-                                <Checkbox name="interest_area" value="advocacy_coalition">Joining or starting a survivor-led policy advocacy coalition in my state</Checkbox>
-                                <Checkbox name="interest_area" value="p2p_financial_support_group">Learning more about Peer to Peer Financial Support Group</Checkbox>
-                                <Checkbox name="interest_area" value="other">Other</Checkbox>
-                            </div>
-                            {props.values.interest_area && props.values.interest_area.indexOf("other") !== -1 &&
+                    {props => {
+                        const formStatus = checkFormStatus(props)
+                        if (formStatus) return formStatus
+                        return (
+                            <Form className="col-12 col-lg-8 mb-5">
+                                <Input label="Your name" name="name" required={ true } />
+                                <Input label="Your pronouns" name="pronouns" required={ true } />
+                                <div className="form-group">
+                                    <FormLabel required={ true }>Which of the following are you interested in? (Select as many as you'd like.)</FormLabel>
+                                    <Checkbox name="interest_area" value="be_policy_advocate">Learning how to be a policy advocate in my state</Checkbox>
+                                    <Checkbox name="interest_area" value="connect_with_survivors">Connecting with other survivors in my state</Checkbox>
+                                    <Checkbox name="interest_area" value="advocacy_coalition">Joining or starting a survivor-led policy advocacy coalition in my state</Checkbox>
+                                    <Checkbox name="interest_area" value="p2p_financial_support_group">Learning more about Peer to Peer Financial Support Group</Checkbox>
+                                    <Checkbox name="interest_area" value="other">Other</Checkbox>
+                                </div>
+                                {props.values.interest_area && props.values.interest_area.indexOf("other") !== -1 &&
                               <Input label="Please describe..." name="interest_area_other" required={ true } />
-                            }
-                            <div className="form-group">
-                                <FormLabel required={ true }>The safest way to contact you is...</FormLabel>
-                                <Checkbox name="contact_method" value="text">Text</Checkbox>
-                                <Checkbox name="contact_method" value="call">Phone call</Checkbox>
-                                <Checkbox name="contact_method" value="email">Email</Checkbox>
-                                <Checkbox name="contact_method" value="none">There is not a safe way to contact me, I will contact you</Checkbox>
-                            </div>
-                            {props.values.contact_method && (props.values.contact_method.indexOf("call") !== -1 || props.values.contact_method.indexOf("text") !== -1) &&
+                                }
+                                <div className="form-group">
+                                    <FormLabel required={ true }>The safest way to contact you is...</FormLabel>
+                                    <Checkbox name="contact_method" value="text">Text</Checkbox>
+                                    <Checkbox name="contact_method" value="call">Phone call</Checkbox>
+                                    <Checkbox name="contact_method" value="email">Email</Checkbox>
+                                    <Checkbox name="contact_method" value="none">There is not a safe way to contact me, I will contact you</Checkbox>
+                                </div>
+                                {props.values.contact_method && (props.values.contact_method.indexOf("call") !== -1 || props.values.contact_method.indexOf("text") !== -1) &&
                               <Input label="Your phone number" name="phone" type="phone" required={ true } />
-                            }
-                            {props.values.contact_method && props.values.contact_method.indexOf("email") !== -1 &&
+                                }
+                                {props.values.contact_method && props.values.contact_method.indexOf("email") !== -1 &&
                               <Input label="Your email" name="email" type="email" required={ true } />
-                            }
-                            {props.values.contact_method && props.values.contact_method.indexOf("none") !== -1 &&
+                                }
+                                {props.values.contact_method && props.values.contact_method.indexOf("none") !== -1 &&
                               <p><em>To contact FreeFrom, please email us at <a href="mailto:info@freefrom.org">info@freefrom.org</a>.</em></p>
-                            }
-                            <Submit />
-                        </Form>
-                    )}
+                                }
+                                <Submit />
+                            </Form>
+                        )
+                    }}
                 </Formik>
                 <TakeAction />
             </SharedLayout>
--- utils/forms.js
+++ utils/forms.js
@@ -1,5 +1,31 @@
+import React from 'react'
+
+export const checkFormStatus = (props) => {
+    // If status has not been updated, form has not yet been submitted.
+    if (!props.status) return null
+    if (props.status.success) {
+        return (
+            <div className="col-12 col-lg-8 mb-5 mb-5 mt-5 alert alert-success" role="alert">
+                <strong>Thanks for your response!</strong> Someone will be in touch with you soon.
+                <a type="button" className="ml-1 alert-link" onClick={props.resetForm}>
+                Submit another response
+                </a>
+            </div>
+        )
+    } else {
+        return (
+            <div className="col-12 col-lg-8 mb-5 mb-5 mt-5 alert alert-danger" role="alert">
+                <strong>Error submitting form!</strong> Please try again later.{" "}
+                <a type="button" className="ml-1 alert-link" onClick={props.resetForm}>
+                  Try again
+                </a>
+            </div>
+        )
+    }
+}
+
 export const submitForm = path => {
-    return async values => {
+    return async (values, {setSubmitting, setErrors, setStatus}) => {
         const res = await fetch(
             `${process.env.NEXT_PUBLIC_API_ENDPOINT}/forms/${path}`,
             {
@@ -12,10 +38,16 @@ export const submitForm = path => {
         )
         try {
             const result = await res.json()
-            console.log(result)
-            alert('Thanks for your response! Someone will be in touch with you soon.')
+            if (res.status >= 400) {
+                const message = result && result.description || 'Unknown error'
+                throw new Error(message)
+            }
+            setStatus({success: true})
         } catch (error) {
-            alert('Error submitting form! Please try again later.')
+            console.log(error)
+            setStatus({success: false})
+            setSubmitting(false)
+            setErrors({submit: error.message})
         }
     }
 }
