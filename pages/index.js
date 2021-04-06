f534b6fb1721ba14a2eb011f9ce45e6f72b99e3a
Sarah Jaisser Green <sjaissergreen@gmail.com>
2021-04-05 21:48:46 -0700

    Move Important Info and Methodology sections to About page
    Closes #214

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,6 @@ import TakeAction from 'components/common/TakeAction'
 import SharedLayout from 'components/SharedLayout'
 import StatesList from 'components/StatesList'
 import UsMap from 'components/UsMap'
-import ModalButton from 'components/modal/ModalButton'
 import Image from 'next/image'
 
 function Home({ states }) {
@@ -101,15 +100,6 @@ function Home({ states }) {
                         </div>
                     </div>
                 </div>
-
-                <h2 className='mt-5'>Important information</h2>
-                <p>The National Survivor Financial Security Policy Map and Scorecard evaluates legislation in each state that has either been passed or enacted. While we understand that there are an endless number of policies and issues that impact survivors’ ability to build wealth, we’ve selected policies that we have determined are most directly linked to a survivor’s ability to build and maintain financial security. This is a living tool, which means it will be regularly updated and expanded. Be sure to read the full methodology to learn more!</p>
-
-                <h2 className='mt-5'>Methodology</h2>
-                <p>We evaluated each state based on whether and to what extent its policies aligned with and provided the same protections as our set of model policies, the characteristics of which are included in each state’s scorecard. The scorecard only includes state-level policies and does not capture local policies or those that can only be addressed on the federal level.</p>
-                <p>While survivors’ ability to build financial security is impacted by a wide range of policies, we chose the thirteen policy categories currently included within our scorecard after determining that they were most directly connected with survivors’ ability to protect their assets and build and save wealth.</p>
-                <p>While practice and policy may differ, the scorecard only measures policies as they are codified in state laws.</p>
-                <ModalButton text="Full Methodology" href="/methodology"/>
 
                 <TakeAction />
 
