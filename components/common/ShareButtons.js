91da0e02d855b93e9f4c1b1f08e081f6270d4f58
Danny (he/him) <DanielKim1@users.noreply.github.com>
2021-04-06 13:16:21 -0400

    Rename freefrom-map-frontend to freefrom-map-frontend-staging (#209)

--- components/common/ShareButtons.js
+++ components/common/ShareButtons.js
@@ -7,7 +7,7 @@ const ShareButtons = ({ className }) => (
     <div className={ className }>
         <a
             className="btn btn-sm btn-primary mr-2"
-            href="https://www.facebook.com/sharer/sharer.php?u=https://freefrom-map-frontend.herokuapp.com/"
+            href="https://www.facebook.com/sharer/sharer.php?u=https://freefrom-map-frontend-prod.herokuapp.com/"
             target="_blank"
             rel="noopener noreferrer"
         >
@@ -15,7 +15,7 @@ const ShareButtons = ({ className }) => (
         </a>
         <a
             className="btn btn-sm btn-primary"
-            href="https://twitter.com/intent/tweet?text=Check%20out%20the%20new%20National%20Survivor%20Wealth%20Policy%20Map%20and%20Scorecard%20from%20@freefromorg!&url=https://freefrom-map-frontend.herokuapp.com/"
+            href="https://twitter.com/intent/tweet?text=Check%20out%20the%20new%20National%20Survivor%20Wealth%20Policy%20Map%20and%20Scorecard%20from%20@freefromorg!&url=https://freefrom-map-frontend-prod.herokuapp.com/"
             target="_blank"
             rel="noopener noreferrer"
         >
