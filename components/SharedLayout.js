96a1be8f48e5e7d049bea6194b18404afde99846
Emily Giurleo <e.m.giurleo@gmail.com>
2021-04-14 22:44:19 -0400

    Add cookies consent page (#226)
    * Add a cookie consent screen where user can accept or decline cookies
    
    * Fix the decline issue
    
    * Revert unnecessary change

--- components/SharedLayout.js
+++ components/SharedLayout.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types"
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import CookiesConsent from 'components/CookiesConsent'
+import { getCookiesFromLocalStorage } from 'utils'
 
 export default function SharedLayout({ children }) {
+    const [cookie, setCookie] = useState(getCookiesFromLocalStorage());
+
     return (
         <div className='shared-layout'>
             <Navbar />
@@ -11,6 +15,7 @@ export default function SharedLayout({ children }) {
                 { children }
             </div>
             <Footer />
+            { cookie != null && cookie != undefined ? null : <CookiesConsent setCookie={setCookie} /> }
         </div>
     );
 }
