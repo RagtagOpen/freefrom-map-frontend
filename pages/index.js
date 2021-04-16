9538c717702ccc03da54df48065bf90fa0526f02
Sarah Jaisser Green <sjaissergreen@gmail.com>
2021-04-16 12:43:31 -0700

    Align snapshot images to center of page (#233)

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,9 @@ function Home({ states }) {
             <>
 
                 <h2 className='mt-4'>Snapshot of Survivor Financial Security Policies By State</h2>
-                <img className="img-fluid" src="/images/snapshot-by-state.png" style={{width: '100%', maxWidth: '1000px'}}/>
+                <div className="d-flex justify-content-center">
+                    <img className="img-fluid" src="/images/snapshot-by-state.png" style={{width: '100%', maxWidth: '1000px'}}/>
+                </div>
 
                 <h2 className='mt-5'>States to Watch</h2>
                 <div className="d-flex justify-content-lg-around justify-content-center align-items-center flex-column flex-md-row p-2">
@@ -90,7 +92,9 @@ function Home({ states }) {
                 </div>
 
                 <h2 className='mt-5'>Snapshot of Survivor Financial Security Policies By Category</h2>
-                <img className="img-fluid" src="/images/snapshot-by-category.png" style={{width: '100%', maxWidth: '1000px'}} />
+                <div className="d-flex justify-content-center">
+                    <img className="img-fluid mb-5" src="/images/snapshot-by-category.png" style={{width: '100%', maxWidth: '1000px'}} />
+                </div>
 
                 <TakeAction />
 
