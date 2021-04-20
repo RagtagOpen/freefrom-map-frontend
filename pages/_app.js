a1dabc0c2fd26bd4357213b387435b852514ca4c
Danny Kim <danielkim1@protonmail.com>
2021-04-19 20:11:20 -0400

    Remove jQuery

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,13 @@
-import { useEffect } from "react"
-
-// CSS
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../styles/globals.css'
+import React from "react";
+import PropTypes from "prop-types";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-    useEffect(() => {
-        import("jquery").then($ => {
-            // jQuery must be installed to the `window`:
-            window.$ = window.jQuery = $;
-            return import("bootstrap");
-        });
-    }, []);
-
-    return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
+
+MyApp.propTypes = {
+    Component: PropTypes.elementType,
+    pageProps: PropTypes.array,
+};
