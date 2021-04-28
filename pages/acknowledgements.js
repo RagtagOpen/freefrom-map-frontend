f16fa85fe64cf6cde3e4972a6e35e259efcf6158
Sarah Jaisser Green <sjaissergreen@gmail.com>
2021-04-28 13:09:28 -0700

    Make Acknowledgments and Disclaimer headers smaller on small screens

--- pages/acknowledgements.js
+++ pages/acknowledgements.js
@@ -7,9 +7,9 @@ export default function Acknowledgements() {
     return (
         <SharedLayout>
             <BackButton className="mt-3 mb-2" />
-            <h1 className="subpage-header">Acknowledgements</h1>
+            <h1 className="ack-header subpage-header">Acknowledgements</h1>
             <p>We want to take a moment to acknowledge that the Policy Map and Scorecard could not have been made possible without the support and contributions made by the law firms, research volunteers and all those involved in this project! We thank you for all of your work and contribution to making this tool a success!</p>
-            <h2>Pro Bono Partners</h2>
+            <h2 className="ack-subheader">Pro Bono Partners</h2>
             <ul className="no-bullets no-indents">
                 <li>Alston & Bird, LLP</li>
                 <li>DLA Piper, LLP</li>
@@ -20,7 +20,7 @@ export default function Acknowledgements() {
                 <li>Seyfarth Shaw, LLP</li>
                 <li>Womble Bond Dickinson (US) LLP</li>
             </ul>
-            <h2>Legal Research Volunteers</h2>
+            <h2 className="ack-subheader">Legal Research Volunteers</h2>
             <ul className="no-bullets no-indents">
                 <li>Dr. Monica Beard</li>
                 <li>Tracy L. Boak, Perlman & Perlman, LLP</li>
@@ -35,15 +35,15 @@ export default function Acknowledgements() {
                 <li>Brooke Thacher</li>
                 <li>Monica Towarnicky</li>
             </ul>
-            <h2>Consultant</h2>
+            <h2 className="ack-subheader">Consultant</h2>
             <ul className="no-bullets no-indents">
                 <li>Priyamvada Kumar</li>
             </ul>
-            <h2>Design</h2>
+            <h2 className="ack-subheader">Design</h2>
             <ul className="no-bullets no-indents">
                 <li>Tiffany Hernandez</li>
             </ul>
-            <h2>Ragtag.org Tech Volunteers</h2>
+            <h2 className="ack-subheader">Ragtag.org Tech Volunteers</h2>
             <ul className="no-bullets no-indents">
                 <li>Jenn Czeck</li>
                 <li>Emily Giurleo</li>
@@ -55,13 +55,13 @@ export default function Acknowledgements() {
                 <li>Grant Stromgren</li>
                 <li>Thaydryan Sweeney</li>
             </ul>
-            <h2>FreeFrom Project Leads</h2>
+            <h2 className="ack-subheader">FreeFrom Project Leads</h2>
             <ul className="no-bullets no-indents">
                 <li>Sabrina Hamm, Policy and Advocacy Specialist</li>
                 <li>Amy Durrence, Director of Systems Change Initiatives</li>
                 <li>Sonya Passi, CEO & Founder</li>
             </ul>
-            <h2>FreeFrom Outreach and Dissemination Team</h2>
+            <h2 className="ack-subheader">FreeFrom Outreach and Dissemination Team</h2>
             <ul className="no-bullets no-indents">
                 <li>Anakaren Alcocer</li>
                 <li>Diana Ayala</li>
