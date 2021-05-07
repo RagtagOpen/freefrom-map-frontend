18d7df0a0c0e6f835ef77f012fefaead1cab3323
Emily Giurleo <e.m.giurleo@gmail.com>
2021-05-07 11:57:39 -0400

    Move ReactGA initialize to tracking utility (#274)

--- utils/tracking.js
+++ utils/tracking.js
@@ -1,6 +1,10 @@
 import ReactGA from "react-ga"
 import { getCookiesFromLocalStorage } from 'utils';
 
+if(process.env.NEXT_PUBLIC_GA_ID) {
+    ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID);
+}
+
 export const trackPageView = (url) => {
     if(getCookiesFromLocalStorage && process.env.NEXT_PUBLIC_GA_ID) {
         ReactGA.pageview(url);
