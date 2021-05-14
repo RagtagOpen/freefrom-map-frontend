81605e9e8a037cd902cf85cf8eb542ee9f6f656d
Emily Giurleo <e.m.giurleo@gmail.com>
2021-05-13 23:14:45 -0400

    test husky again

--- components/StateCard.js
+++ components/StateCard.js
@@ -13,7 +13,7 @@ const StateCard = ({hideLearnMore, state, onLearnMoreClick}) => {
         <div className="card-body p-2 mt-2">
             <h5 className="card-title mb-2 pb-1" style={{textTransform: 'uppercase'}}>{name}</h5>
             <h6 className="card-subtitle mb-2 pb-1 text-muted">Rank: {ordinal(rank)}</h6>
-            <ScoreLabel          score={grade.grade} />
+            <ScoreLabel            score={grade.grade} />
             <p className="card-text mt-2 pt-1 mb-2">
                 {OVERALL_SCORE_LABELS[grade.grade]}
             </p>
